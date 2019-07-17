f60c1724ed181764c9be4e6b126c552abcefc21a
manuel <nelo92@hotmail.com>
2019-07-17 17:41:31 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# spring-boot-angular-blank
+Blank application with spring boot and angular
+
+### Build
+
+```
+mvn package
+```
+
+### Run
+
+```
+java -jar app-server-0.0.1-SNAPSHOT.jar
+```
+
+### Browse
+
+Application
+http://localhost:8080
+
+h2 web ui
+http://localhost:8080/h2
+
+swagge web ui
+http://localhost:8080/swagger-ui.html
---
+++ app-server/src/main/java/fr/ma/myapp/controller/HelloController.java
@@ -0,0 +1,14 @@
+package fr.ma.myapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping
+    public String hello() {
+        return "Hello World!";
+    }
+}
